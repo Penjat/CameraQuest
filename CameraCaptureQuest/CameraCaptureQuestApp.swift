4249bac8f6f369b359f056ec0df0641bada6cb5c
Spencer <37875712+Penjat@users.noreply.github.com>
2021-05-28 21:31:40 -0700

    Initial Commit

---
+++ CameraCaptureQuest/CameraCaptureQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraCaptureQuestApp.swift
+//  CameraCaptureQuest
+//
+//  Created by Spencer Symington on 2021-05-28.
+//
+
+import SwiftUI
+
+@main
+struct CameraCaptureQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
